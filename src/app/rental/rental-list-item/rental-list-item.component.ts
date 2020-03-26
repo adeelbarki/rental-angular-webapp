6f0957ef08c760a805a6ac2fadbc957525099af3
Adeel Ahmed Khan <adeelbarki@gmail.com>
2020-03-26 12:31:55 +0500

    added rental list and items

--- src/app/rental/rental-list-item/rental-list-item.component.ts
+++ src/app/rental/rental-list-item/rental-list-item.component.ts
@@ -4,7 +4,7 @@ import { Input } from '@angular/core';
 @Component({
   selector: 'app-rental-list-item',
   templateUrl: './rental-list-item.component.html',
-  styleUrls: ['./rental-list-item.component.css']
+  styleUrls: ['./rental-list-item.component.scss']
 })
 export class RentalListItemComponent implements OnInit {
 @Input() rental: any;
